554dbd7dd791136bb706e7c0f1ca065ecd82d126
Krishna Sen <krishnasentui@gmail.com>
2022-10-01 01:20:18 +0530

    Create java-basic.java
    all java knowledge

---
+++ java-basic.java
@@ -0,0 +1,92 @@
+import java.util.*;
+
+import java .io.*;
+public class Main
+{
+	public static void main(String[] args) 
+	{
+	    
+// 		System.out.println("Hello World");
+
+
+        // int i = 1;
+        // float f = 3.1f;
+        // char c = 'A';
+        // String s="HI";
+        // boolean b = false;
+// 		System.out.println("int : "+i+"\nfloat : "+f+"\nchar : "+c+"\nString : "+s+"\nboolean : "+b);
+
+        
+        // integer to character
+        // int i = 65;
+        // System.out.println((char)i);
+        
+        // character to integer
+        // char c = 'A';
+        // System.out.println((int)c);
+        
+        // string to integer
+        // String s="123";
+        // System.out.println(Integer.parseInt(s)+1);
+        
+        // integer to string
+        // int s=123;
+        // String s1=Integer.toString(s)+'k';
+        // System.out.println(s1);
+        
+        
+        // Scanner sc=new Scanner(System.in);
+        // String s;
+        // s=sc.nextLine();
+        // System.out.println(s);
+        
+        
+        // Scanner sc=new Scanner(System.in);
+        // char s;
+        // s=sc.next().charAt(0);
+        // System.out.println(s);
+        
+        
+        // integer to binary
+        // int n,s=0,i=0;
+        // Scanner sc=new Scanner(System.in);
+        // n=sc.nextInt();
+        // while(n>0){
+        //     s=((n&1)*(int)Math.pow(10,i++))+s;
+        //     n>>=1;
+        // }
+        // System.out.println(s);
+        
+        
+        // int arr[]={1,2,3,4,5,6,7,8,9};
+        // String s="ht ht";
+        // s+=' ';
+        // System.out.println(arr.length+" "+s.length()+" "+s.charAt(1));
+        
+        
+        // int n=3;
+        // int arr[]=new int[n];
+        // arr[0]=1;
+        // int arr[]={1,2,3,4,5,6,7,8,9};
+        // arr.push(7); //its not working use stack
+        // for(int i=0;i<arr.length;i++){
+            // System.out.println(arr[i]+" ");
+        // }
+        
+        
+        // array fill
+        // int arr[]=new int[5];
+        // Arrays.fill(arr,10);
+        // for(int i=0;i<arr.length;i++){
+        //     System.out.print(arr[i]+" ");
+        // }
+        
+        
+        // int arr[]={12,1,2,3,4,5,6,7,8,9};
+        // Arrays.sort(arr);
+        // System.out.print(arr[9]);
+        
+        
+	}
+}
+
